fb446eb6a491816b5fc258e3f7da6b7d661cb018
matthieumoreau0 <matthieu.moreau@eleves.enpc.fr>
2020-07-28 00:38:51 +0200

    Update test setup script so that it creates an html file to watch the game

--- test_setup.py
+++ test_setup.py
@@ -5,13 +5,17 @@ from kaggle_environments import make
 if __name__=="__main__" :
     
     
-    # Create a test environment for use later
-    board_size = 5
+    # # Create a test environment for use later
+    board_size = 15
     configuration = {"size": board_size, "startingHalite": 1000}
-    environment = make("halite", configuration=configuration)
-    agent_count = 2
-    environment.reset(agent_count)
-    state = environment.state[0]
 
-    actions = base_agent(state.observation, environment.configuration )
+    from kaggle_environments import make
+    env = make("halite", debug=True, configuration=configuration)
+    env.run(["agent.py", "agent.py", "agent.py", "random"])
+    out = env.render(mode="html",width=800, height=600)
+    with open('halite.html', 'w') as f:
+        f.write(out)
+        f.close()
+
+
 
