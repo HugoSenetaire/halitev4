54fbcd3557e3876f246b417458890d2279884af2
matthieumoreau0 <matthieu.moreau@eleves.enpc.fr>
2020-07-28 00:38:06 +0200

    Update base agent to create shipyards when we have none (for testing)

--- agent.py
+++ agent.py
@@ -3,12 +3,12 @@ from random import choice
 import kaggle_environments.envs.halite.helpers as halite
 
 
-def swarm_agent(observation, configuration):
-    """ RELEASE THE SWARM!!! """
-    s_env = adapt_environment(observation, configuration)
-    actions = actions_of_ships(s_env)
-    actions = actions_of_shipyards(actions, s_env)
-    return actions
+# def swarm_agent(observation, configuration):
+#     """ RELEASE THE SWARM!!! """
+#     s_env = adapt_environment(observation, configuration)
+#     actions = actions_of_ships(s_env)
+#     actions = actions_of_shipyards(actions, s_env)
+#     return actions
 
 def base_agent(observation, configuration):
     board = Board(observation,configuration)
@@ -22,9 +22,11 @@ def base_agent(observation, configuration):
         ship.custom_method()
         #The SDK methods also class because our custom class inherits from it
         ship.next_action = choice([halite.ShipAction.NORTH,halite.ShipAction.EAST,halite.ShipAction.SOUTH,halite.ShipAction.WEST,None])
+        if len(me.shipyards)<1:
+            ship.next_action = halite.ShipAction.CONVERT
 
     # Set actions for each shipyard
     for shipyard in me.shipyards:
-        shipyard.next_action = None
+        shipyard.next_action = halite.ShipyardAction.SPAWN
 
     return me.next_actions
